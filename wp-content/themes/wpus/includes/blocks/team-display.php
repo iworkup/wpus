d502fac3522c0f9b7714f492a1fe1f7546a51378
Igor Dmitriev <iworkup@ya.ru>
2020-07-03 11:05:21 +0300

    modify example block, add comments

--- wp-content/themes/wpus/includes/blocks/team-display.php
+++ wp-content/themes/wpus/includes/blocks/team-display.php
@@ -1,30 +1,37 @@
-<?php if (get_field('field_team_title')) : ?>
+<?php if (get_field('team_title')) : ?>
 
-    <p><?= get_field('field_team_title') ?></p>
-
-<?php endif; ?>
-
-<?php if (get_field('field_team_subtitle')) : ?>
-
-    <p><?= get_field('field_team_subtitle') ?></p>
+    <p><?= get_field('team_title') ?></p>
 
 <?php endif; ?>
 
-<?php if (get_field('field_team_items')) : ?>
+<?php if (get_field('team_subtitle')) : ?>
 
+    <p><?= get_field('team_subtitle') ?></p>
 
-    <?php foreach (get_field('field_team_items') as $item) : ?>
+<?php endif; ?>
 
-        <?php if ($item['field_team_name']) : ?>
-            <p><?= $item['field_team_name'] ?></p>
+<?php if (get_field('team_items')) : ?>
+
+    <?php foreach (get_field('team_items') as $item) : ?>
+
+        <?php if ($item['team_name']) : ?>
+            <p><?= $item['team_name'] ?></p>
         <?php endif; ?>
 
-        <?php if ($item['field_team_publish_no']) : ?>
-            <p><?= $item['field_team_publish_no'] ?></p>
+        <?php if ($item['team_publish'] === 'no') : ?>
+
+            <?php if ($item['team_publish_no']) : ?>
+                <p><?= $item['team_publish_no'] ?></p>
+            <?php endif; ?>
+
         <?php endif; ?>
 
-        <?php if ($item['field_team_publish_yes']) : ?>
-            <p><?= $item['field_team_publish_yes'] ?></p>
+        <?php if ($item['team_publish'] === 'yes') : ?>
+
+            <?php if ($item['team_publish_yes']) : ?>
+                <p><?= $item['team_publish_yes'] ?></p>
+            <?php endif; ?>
+
         <?php endif; ?>
 
     <?php endforeach; ?>
--- wp-content/themes/wpus/includes/blocks/team.php
+++ wp-content/themes/wpus/includes/blocks/team.php
@@ -12,9 +12,7 @@ if (function_exists('acf_register_block_type')) {
             'name' => 'team-block', // нельзя использовать нижние подчёркивания
             'title' => 'Команда',
             'render_template' => 'includes/blocks/team-display.php',
-//            'render_template' => render(),
             'category' => 'faq-category',
-            'icon' => '',
             'mode' => 'edit',
         ));
 
@@ -35,32 +33,32 @@ if (function_exists('acf_add_local_field_group')) {
             array(
                 'key' => 'field_team_title', // обязательно использовать приставку field_
                 'label' => 'Заголовок', // значение не должно совпадать со значением ключа key
-                'name' => 'Заголовок', // значение не должно совпадать со значением ключа key
+                'name' => 'team_title', // значение не должно совпадать со значением ключа key; по этому значению происходит вызов значения поля на конечной странице
                 'type' => 'text',
             ),
             array(
                 'key' => 'field_team_subtitle',
                 'label' => 'Подаголовок',
-                'name' => 'Подаголовок',
+                'name' => 'team_subtitle',
                 'type' => 'text',
             ),
             array(
                 'key' => 'field_team_items',
                 'label' => 'Команда',
-                'name' => 'Команда',
+                'name' => 'team_items',
                 'type' => 'repeater',
                 'layout' => 'block',
                 'sub_fields' => array(
                     array(
                         'key' => 'field_team_name',
                         'label' => 'Имя',
-                        'name' => 'Имя',
+                        'name' => 'team_name',
                         'type' => 'text',
                     ),
                     array(
                         'key' => 'field_team_publish',
                         'label' => 'Показывать?',
-                        'name' => 'Показывать?',
+                        'name' => 'team_publish',
                         'type' => 'radio',
                         'choices' => array(
                             'no' => 'Нет',
@@ -70,7 +68,7 @@ if (function_exists('acf_add_local_field_group')) {
                     array(
                         'key' => 'field_team_publish_no',
                         'label' => 'Нет',
-                        'name' => 'Нет',
+                        'name' => 'team_publish_no',
                         'type' => 'text',
                         'conditional_logic' => array(
                             array(
@@ -85,7 +83,7 @@ if (function_exists('acf_add_local_field_group')) {
                     array(
                         'key' => 'field_team_publish_yes',
                         'label' => 'Да',
-                        'name' => 'Да',
+                        'name' => 'team_publish_yes',
                         'type' => 'text',
                         'conditional_logic' => array(
                             array(
@@ -105,7 +103,7 @@ if (function_exists('acf_add_local_field_group')) {
                 array(
                     'param' => 'block',
                     'operator' => '==',
-                    'value' => 'acf/team-block',
+                    'value' => 'acf/team-block', // приставка acf/ обязательна
                 ),
             ),
         ),
@@ -113,14 +111,102 @@ if (function_exists('acf_add_local_field_group')) {
 
 }
 
-function render()
-{
+/*
 
-    return <<<EOT
+Типы полей (type)
 
-{var_dump(get_field('field_team_title'))}
+Основное
 
-EOT;
+    text _ Текст
+    textarea _ Область текста
+    number _ Число
+    range _ Диапазон
+    email _ E-mail
+    url _ Ссылка
+    password _ Пароль
 
+Содержание
 
-}
+    image _ Изображение
+    file _ Файл
+    wysiwyg _ Редактор WordPress
+    oembed _ Медиа
+    gallery _ Галерея
+
+Выбор
+
+    select _ Выбор (select)
+    checkbox _ Флажок (checkbox)
+    radio _ Переключатель (radio)
+    button_group _ Группа кнопок
+    true_false _ Да / Нет
+
+Отношение
+
+    link _ Ссылка
+    post_object _ Объект записи
+    page_link _ Ссылка на страницу
+    relationship _ Записи
+    taxonomy _ Таксономия
+    user _ Пользователь
+
+jQuery
+
+    google_map _ Расположение на карте
+    date_picker _ Дата
+    date_time_picker _ Дата и время
+    time_picker _ Время
+    color_picker _ Цвет
+
+Блок
+
+    message _ Сообщение
+    accordion _ Аккордеон
+    tab _ Вкладка
+    group _ Группа
+    repeater _ Повторитель
+    flexible_content _ Гибкое содержание
+    clone _ Клон
+
+*/
+
+/*
+
+Привязка полей к типу занных (location)
+
+Запись
+
+    post_type _ Тип записи
+    post_template _ Шаблон записи
+    post_status _ Статус записи
+    post_format _ Формат записи
+    post_category _ Рубрика записи
+    post_taxonomy _ Таксономия записи
+    post _ Запись
+
+Страница
+
+    page_template _ Шаблон страницы
+    page_type _ Тип страницы
+    page_parent _ Родитель страницы
+    page _ Страница
+
+Пользователь
+
+    current_user _ Текущий пользователь
+    current_user_role _ Группа текущего пользователя
+    user_form _ Пользователь
+    user_role _ Группа пользователя
+
+Формы
+
+    taxonomy _ Таксономия
+    attachment _ Медиафайл
+    comment _ Комментарий
+    widget _ Виджет
+    nav_menu _ Меню
+    nav_menu_item _ Пункт меню
+    block _ Блок
+    options_page _ Страница с опциями
+
+*/
